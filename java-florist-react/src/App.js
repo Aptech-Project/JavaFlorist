80591231cec7cae973e1ca9b1ae4809996553b2a
SimpleCray <54101094+SimpleCray@users.noreply.github.com>
2021-01-14 00:47:32 +0700

    init template

--- java-florist-react/src/App.js
+++ java-florist-react/src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 import { Route, Switch, Redirect, useLocation } from 'react-router-dom';
 import { Home } from './components/layout/Home';
--- java-florist-react/src/index.js
+++ java-florist-react/src/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
+
+import "assets/scss/material-kit-react.scss?v=1.9.0";
 import { Provider } from "react-redux";
-import { store } from './reducers/store';
+// pages for this product
+import Components from "views/Components/Components.js";
+import LandingPage from "views/LandingPage/LandingPage.js";
+import ProfilePage from "views/ProfilePage/ProfilePage.js";
+import LoginPage from "views/LoginPage/LoginPage.js";
+import { store } from "reducers/store";
+
+var hist = createBrowserHistory();
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Router history={hist}>
+      <Switch>
+        <Route path="/landing" component={LandingPage} />
+        <Route path="/profile" component={ProfilePage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/" component={Components} />
+      </Switch>
+    </Router>
   </Provider>,
-  
-  document.getElementById('root')
+  document.getElementById("root")
 );
