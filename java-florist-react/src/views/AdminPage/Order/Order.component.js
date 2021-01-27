6913cdce69d49550917c98ff425f952b1a6a3fcd
phongnguyen1003 <nguyenxuan.phong.1989@gmail.com>
2021-01-27 12:45:28 +0700

    Update list order admin

--- java-florist-react/src/views/AdminPage/Order/Order.component.js
+++ java-florist-react/src/views/AdminPage/Order/Order.component.js
@@ -35,7 +35,7 @@ const Messagecomponent = ({ classes }) => {
                 <GridItem xs={12} sm={12} md={12}>
                     <Card>
                         <CardHeader color="primary">
-                            <h4 className={classes.cardTitleWhite}>LIST OF USER</h4>
+                            <h4 className={classes.cardTitleWhite}>LIST OF ORDER</h4>
                         </CardHeader>
                         <CardBody>
                             <GridContainer>
